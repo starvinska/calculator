26ce7d5ce134f51ef389bf21d4bedfa56fb9e34c
starvinska <olajochemczyk@gmail.com>
2022-07-02 08:41:26 +0200

    basic calculator functionality

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -16,13 +16,56 @@
 //     console.log(e)
 // } )
 
-const input1 = document.querySelector('.first')
+// const input1 = document.querySelector('.first')
 
-const b = document.querySelector ('.numbers')
+// const b = document.querySelector ('.numbers')
 
-b.addEventListener('click', (e) => {
-    if(e.target.classList.contains('num')){
-        input1.value=e.target.innerText
+// b.addEventListener('click', (e) => {
+//     if(e.target.classList.contains('num')){
+//         input1.value=e.target.innerText
+//     }
+// })
+
+const mathSymbol = document.querySelector('.plus')
+
+const actionsContainer = document.querySelector('.actions')
+
+const firstInput = document.querySelector('.first')
+
+const secondInput =document.querySelector('.second')
+
+const resultInput =document.querySelector('.result')
+
+let firstInputValue 
+
+let secondInputValue 
+
+let operation = '+'
+
+mathSymbol.innerText = operation
+
+actionsContainer.addEventListener('click', (event) => {
+    if(event.target.classList.contains('math') && event.target.classList.contains('math-equal') === false ) {
+        mathSymbol.innerText=event.target.innerText
+        operation = event.target.innerText
+    } 
+    else if(event.target.classList.contains('math-equal') && firstInputValue && secondInputValue ) {
+        if(operation==='+') {
+            resultInput.value= firstInputValue + secondInputValue
+        } else if(operation==='-') {
+            resultInput.value= firstInputValue - secondInputValue
+        } else if(operation===':') {
+            resultInput.value= firstInputValue / secondInputValue
+        } else if(operation==='*') {
+            resultInput.value= firstInputValue * secondInputValue
+        }
     }
+} )
+
+firstInput.addEventListener('input', (event) => {
+    firstInputValue = +event.target.value
 })
 
+secondInput.addEventListener('input', (event) => {
+    secondInputValue = +event.target.value
+})
